6c8a0c8999f3a963156f51063a2c113effbc6033
TJ7755 <tj7755@duck.com>
2026-06-05 19:12:08 +0100

    chore(tooling): add ESLint and Prettier configuration
    Add .eslintrc.cjs with React, TypeScript, and hooks rules including
    no-floating-promises and exhaustive-deps as errors. Add .prettierrc
    matching the project's existing style. Add lint, lint:fix, and format
    scripts to package.json.

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'eqeqeq': ['error', 'always'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'no-console': ['warn', { allow: ['error'] }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
